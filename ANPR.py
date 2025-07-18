8ad0e39108e519c5ab2cdbff9eb585b43eac2447
siddhant2397 <67710961+siddhant2397@users.noreply.github.com>
2025-07-19 01:53:12 +0530

    Update ANPR.py

--- ANPR.py
+++ ANPR.py
@@ -49,7 +49,7 @@ if uploaded_file is not None:
 
 # Show just the plate number (robustly):
             plate_val = (
-                response.raw_http.get("inference", {})
+                data.raw_http.get("inference", {})
                 .get("result", {})
                 .get("fields", {})
                 .get("license_plate", {})
