4538fe87b3f13fba4ca431776420819a3eeb6a73
siddhant2397 <67710961+siddhant2397@users.noreply.github.com>
2025-10-14 04:00:37 +0530

    Update ANPR.py

--- ANPR.py
+++ ANPR.py
@@ -17,7 +17,7 @@ client = pymongo.MongoClient(mongo_uri)
 db = client["anpr_database"]
 collection = db["search_logs"]
 
-model_id = "034cfb85-6063-4f29-b6a9-97b00d528337"  
+model_id = "6673789a-7589-4027-b9e2-6995470dbeab"  
 
 st.title("Automatic Number Plate Recognition")
 
@@ -84,7 +84,7 @@ if uploaded_file is not None and authorized_plates and location:
                 data.get("inference", {})
                 .get("result", {})
                 .get("fields", {})
-                .get("number_plate_text", {})
+                .get("license_plate_number", {})
                 .get("value", None)
             )
 
